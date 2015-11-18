9d62315babf046d8422d562c70a2d52f2c014bba
Gavyn <gavyn.riebau@gmail.com>
2015-11-18 18:54:40 +0800

    Working drawing of moustache

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({ dest: 'public/uploads/', limits: { fileSize: 5000000 } });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Moustaches' });
+});
+
+router.post('/', upload.single('image'), function(req, res, next) {
+  res.render('draw', { title: 'Moustaches', name: req.file.filename });
 });
 
 module.exports = router;
